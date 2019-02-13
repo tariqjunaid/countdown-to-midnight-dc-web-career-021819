587e23676a71c119ccb825d939d3a34a8a17cf52
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-13 01:19:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,3 @@
-#write your code here
-def LOOPS
 def countdown(num_secs)
   num = num_secs
     while num > 0
@@ -8,7 +6,7 @@ def countdown(num_secs)
   end
   return "HAPPY NEW YEAR!"
 end
-#countdown(10)
+
 
 def countdown_with_sleep(num_secs)
   num = num_secs
@@ -18,6 +16,4 @@ def countdown_with_sleep(num_secs)
     sleep 1
   end
   puts "HAPPY NEW YEAR!"
-end
-#countdown_with_sleep(10)
 end
