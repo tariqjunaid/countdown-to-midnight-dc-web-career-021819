7d2f53c3b70fc14eb82bf333bdf7b1892d44991c
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-13 01:16:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
-
-def countdown
+def countdown(num_secs)
+  num = num_secs
+    while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+#countdown(10)
+
+def countdown_with_sleep(num_secs)
+  num = num_secs
+    while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep 1
+  end
+  puts "GOOD MORNING SAFA! ♥️♥️♥️I LOVE YOU ♥️♥️♥️"
+end
+countdown_with_sleep(10)
